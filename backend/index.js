a1a07f1d8be7c1b3528ef42db0a07c353c96a663
Rafael Freitas <49541181+Rafonel@users.noreply.github.com>
2020-03-25 18:46:10 -0300

    added nodemon, Knex and SQLite

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
+// MAKE A RESPONSE JSON
+app.use(express.json());
 
-app.get('/', (req,res) => {
-    return res.json({"type":"sucess", "data":"Corona defeated"})
-})
+/* 
 
-app.listen(3333)
+QUERY ??? PARAMS -> GET,  ( filters, pagination)
+ROUTE //// PARAMS -> Identify :id
+REQUEST BODY 
+
+*/
+
+app.post("/users", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  return res.json({
+    type: "sucess",
+    data: "Corona defeated"
+  });
+});
+
+app.listen(3333,'', () => console.log("Server is running on port: 3333"));

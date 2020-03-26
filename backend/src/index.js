59c879bd8691670c7196b48acafcc595ab30d4b9
Rafael Freitas <49541181+Rafonel@users.noreply.github.com>
2020-03-26 00:20:37 -0300

    Added CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 const express = require("express")
+const cors = require('cors');
 const routes = require('./routes')
-
 const app = express();
-
+// cors = security
+app.use(cors());
 // MAKE A RESPONSE JSON
 app.use(express.json());
 app.use(routes);

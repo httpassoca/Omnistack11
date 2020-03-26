4c1dc6a1458e97b721ce3e8da65d8b1c7970a075
Rafael Freitas <49541181+Rafonel@users.noreply.github.com>
2020-03-26 00:21:49 -0300

    Edited Incident.index function to show about the ongs too

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,13 +1,28 @@
 const connection = require("../database/connection");
 module.exports = {
-
   /*
     index => list all incidents
     create => create incidents
     delete => delete incidents
   */
   async index(req, res) {
-    const incidents = await connection("incidents").select("*");
+    const { page = 1 } = req.query;
+    const [count] = await connection("incidents").count();
+    console.log(count);
+    // Picking data from incidents and their respectivies ongs
+    const incidents = await connection("incidents")
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        "incidents.*",
+        "ongs.name",
+        "ongs.email",
+        "ongs.whatsapp",
+        "ongs.city",
+        "ongs.uf"
+      ]);
+    res.header("X-Total-Count", count["count(*)"]);
     return res.json(incidents);
   },
 
@@ -27,13 +42,18 @@ module.exports = {
     const { id } = req.params;
     const ong_id = req.headers.authorization;
     // Verify if the Incident
-    const incident = await connection('incidents').where('id',id).select('ong_id').first();
+    const incident = await connection("incidents")
+      .where("id", id)
+      .select("ong_id")
+      .first();
     // If the Incident ong_id doesnt match with the authorization id from the ong connected
-    if(incident.ong_id != ong_id){
-        // 401 == Failed because the operation are not permitted
-        return res.status(401).json({error: "Operation not permitted"});
+    if (incident.ong_id != ong_id) {
+      // 401 == Failed because the operation are not permitted
+      return res.status(401).json({ error: "Operation not permitted" });
     }
-    await connection('incidents').where('id',id).delete();
+    await connection("incidents")
+      .where("id", id)
+      .delete();
     // 204 == Success but no content to return
     return res.status(204).send();
   }

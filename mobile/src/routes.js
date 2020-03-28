1d563b3a55261a5c85badf17e3fa02294c91c276
Rafael Freitas <49541181+Rafonel@users.noreply.github.com>
2020-03-28 15:00:59 -0300

    Added Incidents page on mobile app

---
+++ mobile/src/routes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+const AppStack = createStackNavigator();
+
+import Incidents from "./pages/Incidents";
+import Detail from "./pages/Detail";
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{headerShown: false}}>
+        <AppStack.Screen component={Incidents} name="Incidents" />
+        <AppStack.Screen component={Detail} name="Detail" />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
